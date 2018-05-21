a33110cee4df9a32b9d6a14711a0ffbc1f45f6cb
Kelly Gibbens <gibbenskd@gmail.com>
2018-05-21 14:11:22 -0500

    Initial commit.

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const speech = document.querySelector('.speech');
+speech.appendChild(p);
+  
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+      p.textContent = transcript;
+
+      if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        speech.appendChild(p);
+        }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
